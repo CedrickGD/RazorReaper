b5ef8ee98f8bd902c008d5de0eebb9bedcc9f207
CedrickGD <business.grabe@gmail.com>
2025-07-30 15:00:12 +0200

    Removed Running Version App Title

--- RazorReaper/App.xaml.cs
+++ RazorReaper/App.xaml.cs
@@ -9,7 +9,7 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new MainPage()) { Title = "RazorReaper Running: v1.0.0" };
+            return new Window(new MainPage()) { Title = "RazorReaper" };
         }
     }
 }
